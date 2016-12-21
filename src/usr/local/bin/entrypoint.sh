8eb86fc0a87c562db574412f596e61bf8d61306a
Flavio Aiello <flavio.aiello@swisscom.com>
2016-12-21 10:35:39 +0100

    added client management based on client names

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -1,36 +1,53 @@
 #!/bin/sh
 
-if [ ${INIT} ];then
-   rm -rf ${OPENVPN}/*
+if [ ! -f ${OPENVPN}/server.conf ]; then
+
+   # Remove old pki
+   rm -rf ${EASYRSA_PKI}
+   touch ${OPENVPN}/crl.pem
+
+   # Init new pki
    easyrsa init-pki
    easyrsa --batch --req-cn="server" build-ca nopass
    easyrsa gen-dh
+
+   # Create server cert
    easyrsa build-server-full server nopass
-   easyrsa build-client-full client nopass
+
+   # Write server config file
+   awk '/\{\{ CA \}\}/{system("cat ${EASYRSA_PKI}/ca.crt");next};/\{\{ CERT \}\}/{system("cat ${EASYRSA_PKI}/issued/server.crt");next};/\{\{ KEY \}\}/{system("cat ${EASYRSA_PKI}/private/server.key");next};/\{\{ DH \}\}/{system("cat ${EASYRSA_PKI}/dh.pem");next}1' /tmp/server.tmpl > ${OPENVPN}/server.conf
 fi
 
-cat > ${OPENVPN}/server.conf <<-EOF
-proto udp
-port 1194
-mode server
-tls-server
-topology subnet
-push "topology subnet"
-dev tun
-tun-ipv6
-float
-comp-lzo
-server 192.168.254.0 255.255.255.0
-user nobody
-group nobody
-persist-key
-persist-tun
-keepalive 10 120
-reneg-sec 0
-ca ${EASYRSA_PKI}/ca.crt
-cert ${EASYRSA_PKI}/issued/server.crt
-key ${EASYRSA_PKI}/private/server.key
-dh ${EASYRSA_PKI}/dh.pem
-EOF
+# Create clients certs
+for CLIENT in ${CLIENTS}; do
 
+   # Generate key and write file if client is not existant
+   if [ ! -f ${OPENVPN}/${CLIENT}.conf ]; then
+
+      # Generate client config
+      easyrsa build-client-full ${CLIENT} nopass
+
+      # Write config file
+      awk '/\{\{ SERVICE_URL \}\}/{print $1 " '${SERVICE_URL}'";next};/\{\{ CA \}\}/{system("cat ${EASYRSA_PKI}/ca.crt");next};/\{\{ CERT \}\}/{system("cat ${EASYRSA_PKI}/issued/'${CLIENT}'.crt");next};/\{\{ KEY \}\}/{system("cat ${EASYRSA_PKI}/private/'${CLIENT}'.key");next}1' /tmp/client.tmpl > ${OPENVPN}/${CLIENT}.conf
+
+      # Add entry to list
+      echo ${CLIENT} >> ${OPENVPN}/clients.lst
+   fi
+done
+
+# Revoke removed clients and cleanup client profiles
+REVOKES=$(for REVOKED in ${CLIENTS} $(cat ${OPENVPN}/clients.lst); do echo "$REVOKED"; done | sort | uniq -u)
+for REVOKE in ${REVOKES}; do
+   echo yes | easyrsa revoke ${REVOKE}
+   rm ${OPENVPN}/${REVOKE}.conf
+done
+easyrsa gen-crl
+
+# Update the client list
+rm ${OPENVPN}/clients.lst
+for CLIENT in ${CLIENTS}; do echo ${CLIENT} >> ${OPENVPN}/clients.lst; done
+
+echo "*** Your client keys are stored in ${OPENVPN} ***"
+
+# Start openvpn
 exec "$@"
